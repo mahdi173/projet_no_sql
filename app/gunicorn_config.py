e64071b2cea7bacfbc1561ddf7e337313c774776
mahdi173 <zougarimehdi44@gmail.com>
2026-02-18 13:53:22 +0100

    feat: update prod config

--- app/gunicorn_config.py
+++ app/gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 import os
 
 # Gunicorn configuration
-bind = "0.0.0.0:5000"
+bind = f"0.0.0.0:{os.getenv('PORT', '5622')}"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
 timeout = 120
